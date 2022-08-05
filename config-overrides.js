40272415a1b44170a019a58b3f9010bcda204229
xieyuhang <13451213869@163.com>
2022-08-05 12:21:33 +0800

    fix deploy

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,15 @@
 const path = require('path')
 
+console.log(process.env.NODE_ENV)
+
 /**
  * @param {import('webpack').Configuration} config 
  * @returns 
  */
 module.exports = function override(config) {
+  if (process.env.NODE_ENV === 'production') {
+    config.output.publicPath = '/react-use/'
+  }
   config.resolve.alias['@'] = path.resolve(__dirname, 'src/')
-
   return config;
 }
